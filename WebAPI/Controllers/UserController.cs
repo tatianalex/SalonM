46f70fae33e3b55e92c1c36da6f112d95e3a1fb5
tatianalex <127817082+tatianalex@users.noreply.github.com>
2023-06-08 15:28:14 +0300

    Add files via upload

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Enums;
+using Services.Interfaces;
+using Services.Services.UsersServices;
+
+namespace API.Controllers;
+
+
+/// <summary>
+/// Авторизация
+/// </summary>
+[Route("/[controller]")]
+[Produces("application/json")]
+public class AuthController : Microsoft.AspNetCore.Mvc.Controller
+{
+    [HttpPost]
+    [Route($"{nameof(Login)}")]
+    [ProducesResponseType(200, Type = typeof(BaseResponse<UserResponse>))]
+    [ProducesResponseType(400, Type = typeof(BaseResponse))]
+    public async Task<BaseResponse<UserResponse>> Login([FromServices] IUserService auth, [FromQuery] UserRequest request)
+    {
+        return await auth.Login(request);
+    }
+
+    [HttpPut]
+    [Route($"{nameof(Register)}")]
+    [ProducesResponseType(200, Type = typeof(BaseResponse))]
+    [ProducesResponseType(400, Type = typeof(BaseResponse))]
+    public async Task<BaseResponse> Register([FromServices] IUserService auth, [FromQuery] RegisterRequest request)
+    {
+        return await auth.Register(request);
+    }
+
+    [HttpPatch]
+    [Route($"{nameof(ActivateAccount)}")]
+    [ProducesResponseType(200, Type = typeof(BaseResponse<TokenResponse>))]
+    [ProducesResponseType(400, Type = typeof(BaseResponse))]
+    public async Task<BaseResponse> ActivateAccount([FromServices] IUserService auth, [FromBody] ActivateAccountRequest request)
+    {
+        return await auth.ActivateAccount(request);
+    }
+}
